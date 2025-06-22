22143786685097cd153aa8312039cf2bbe2c9cd5
andfb <anderson.pereira@fieb.edu.br>
2025-06-22 16:54:00 -0300

    webapi commit

---
+++ webapi_carros/Model/CarroModel.cs
@@ -0,0 +1,11 @@
+namespace webapi_carros.Models;
+
+public class CarroModel
+{
+    
+    public int Id { get; set; }
+
+    public String? Nome { get; set; }
+    public String? Fabricante { get; set; }
+
+}
